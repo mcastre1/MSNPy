44e7962371b2500fc5ad3bc64ddb8aabd98a58bb
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-05-22 12:44:17 -0600

    Button and text box working together.

--- client.py
+++ client.py
@@ -4,7 +4,16 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 class MainScreen(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
+    def say_hello(self):
+        #print("Hello World")
+        #self.ids.my_text.text = "Hi" # This is how we access the ids from a kv file.
+        if self.ids.my_text.text: # Check if there is actually something in the text box before sending.
+            print(self.ids.my_text.text)
+            self.ids.my_text.text = ""
+        
 
 class ClientApp(App):
     def build(self):

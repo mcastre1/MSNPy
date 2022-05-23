c0c62899b4bbf045817c4d02d883f97cc832b003
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-05-22 21:20:14 -0600

    Server receives message from client.

--- client.py
+++ client.py
@@ -2,19 +2,56 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
+import socket
 
+# Infomartion needed for connecting to the server ##
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = '192.168.1.15'
+ADDR = (SERVER, PORT)
+####################################################
+
+# Connecting client to server ##########
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+########################################
+
+"""
+Client GUI class.
+"""
 class MainScreen(Widget):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
+        Window.bind(on_request_close=self.end_func) # Binding the close button on a window to a function (end_func)
+    
+    # Takes care of window close button event.
+    # Will send disconnect message to server to make sure we disconnect from server properly.
+    def end_func(self, *args):
+        self.send(DISCONNECT_MESSAGE)
+        Window.close()
 
-    def say_hello(self):
-        #print("Hello World")
-        #self.ids.my_text.text = "Hi" # This is how we access the ids from a kv file.
-        if self.ids.my_text.text: # Check if there is actually something in the text box before sending.
-            print(self.ids.my_text.text)
-            self.ids.my_text.text = ""
+    # Intermediary function to send a message to the server.
+    def send_message(self):
+        msg = self.ids.my_text.text
+        self.send(msg)
+
+    # Sends message to server.
+    def send(self,msg):
+        if self.ids.my_text.text:
+            message = msg.encode(FORMAT) # Encode into byte format first.
+            msg_length = len(msg)
+            send_length = str(msg_length).encode('utf-8')
+            send_length += b' ' * (HEADER - len(send_length)) # Pads message length to make sure it folows the HEADER/FORMAT of 64 in this case.
+            client.send(send_length)
+            client.send(message)
+            print(client.recv(2048).decode(FORMAT))
         
-
+"""
+App class for Kivy dev.
+"""
 class ClientApp(App):
     def build(self):
         return MainScreen()
